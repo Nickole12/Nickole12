8716c0a9a9233583a35da08b2b47faadae7de894
Nickole12 <89548804+Nickole12@users.noreply.github.com>
2021-09-10 21:51:26 -0300

    Update and rename criando_um_repositorio to criando_umaTabuadaC#.cs

---
+++ criando_umaTabuadaC#.cs
@@ -0,0 +1,124 @@
+using System;
+
+   class principal{
+
+static void Main(string[]args){
+	
+      int numero = 2;
+      int numero2 = 3;
+      int numeric = 4;
+      int casa_cinco = 5;
+      int sesta_casa = 6;
+      int cas_setima = 7;
+
+
+  Console.WriteLine("_______________________________");
+      Console.WriteLine("         #TABUADA#     ");
+      
+      // Inpremssâo da casa de 2,3 e 4//
+      
+  Console.WriteLine("_______________________________");
+  Console.Write("2x1 = {0} |", numero * 1);
+  Console.Write("3x1 = {0} |", numero2 * 1);
+  Console.WriteLine(" 4x1 = {0}", numeric * 1);
+  
+  
+  Console.Write("2x2 = {0} |", numero * 2);
+  Console.Write("3x2 = {0} |",numero2 * 2);
+  Console.WriteLine(" 4x2 = {0}", numeric * 2);
+  
+  Console.Write("2x3 = {0} |", numero * 3);
+  Console.Write("3x3 = {0} |", numero2 * 3);
+  Console.WriteLine(" 4x3 = {0}", numeric * 3);
+   
+  Console.Write("2x4 = {0} |", numero * 4);
+  Console.Write("3x4 = {0}| ", numero2 * 4);
+  Console.WriteLine("4x4 = {0}", numeric * 4);
+  
+  
+  Console.Write("2x5 = {0}|", numero * 5);
+  Console.Write("3x5 = {0}| ", numero2 * 5);
+  Console.WriteLine("4x5 = {0}", numeric * 5);
+  
+  Console.Write("2x6 = {0}|", numero * 6);
+  Console.Write("3x6 = {0}| ", numero2 * 6);
+  Console.WriteLine("4x6 = {0}", numeric *4);
+  
+  
+  Console.Write("2x7 = {0}|", numero * 7);
+  Console.Write("3x7 = {0}| ", numero2 * 7);
+  Console.WriteLine("4x7 = {0}", numeric * 7);
+  
+  
+  Console.Write("2x8 = {0}|", numero * 8);
+  Console.Write("3x8 = {0}| ", numero2 * 8);
+  Console.WriteLine("4x8 = {0}", numeric * 8);
+  
+  
+  Console.Write("2x9 = {0}|", numero *9);
+  Console.Write("3x9 = {0}|", numero2 * 9);
+  Console.WriteLine(" 4x9 = {0}", numeric * 9);
+
+  Console.Write("2x10= {0}|", numero * 10);
+  Console.Write("3x10= {0}| ", numero2 * 10);
+  Console.WriteLine("4x10= {0}", numeric * 10);
+
+Console.Write("_______________________________");
+	
+	// Imprenssão da casa de 5,6 e 7//
+
+  Console.Write("5x1 = {0} |", casa_cinco * 1);
+  Console.Write("6x1 = {0} | ", sesta_casa * 1);
+  Console.WriteLine("7x1 = {0}", cas_setima*1);
+ 
+  Console.Write("5x2 = {0}|", casa_cinco* 2);
+  Console.Write("6x2 ={0} | ", sesta_casa*2);
+  Console.WriteLine("7x2 = {0}", cas_setima*2);
+	
+  Console.Write("5x3 = {0}|", casa_cinco*3);
+  Console.Write("6x3 = {0}|", sesta_casa*3);
+  Console.WriteLine(" 7x3 = {0} ", cas_setima*3);
+  
+  Console.Write("5x4 = {0}|", casa_cinco*4);
+  Console.Write("6x4 = {0}|", sesta_casa*4);
+  Console.WriteLine(" 7x4 = {0}", cas_setima*4);
+  
+  Console.Write("5x5 = {0}|", casa_cinco*5);
+  Console.Write("6x5 = {0}|", sesta_casa*5);
+  Console.WriteLine(" 7x5 = {0}", cas_setima*5);
+	
+  Console.Write("5x6 = {0}|", casa_cinco*6);
+  Console.Write("6x6 = {0}|", sesta_casa*6);
+  Console.WriteLine(" 7x6 = {0}", cas_setima*6);
+	
+  Console.Write("5x7 = {0}|", casa_cinco*7);
+  Console.Write("6x7 = {0}|", sesta_casa*7);
+  Console.WriteLine(" 7x7 = {0}", cas_setima*7);
+	
+
+  Console.Write("5x8 = {0}|", casa_cinco*8);
+  Console.Write("6x8 = {0}|", sesta_casa*8);
+  Console.WriteLine(" 7x8 = {0}", cas_setima*8);
+  
+  Console.Write("5x9 = {0}|", casa_cinco*9);
+  Console.Write("6x9 = {0}|", sesta_casa*9);
+  Console.WriteLine(" 7x9 = {0}", cas_setima*9);
+  
+  Console.Write("5x10= {0}|", casa_cinco*10);
+  Console.Write("6x10= {0}|", sesta_casa*10);
+  Console.WriteLine(" 7x10= {0} ", cas_setima*10);
+  
+Console.Write("_______________________________");
+  
+  
+  
+  
+}   	
+   	
+   	
+   	
+   	
+   	
+   	
+   	
+   }
